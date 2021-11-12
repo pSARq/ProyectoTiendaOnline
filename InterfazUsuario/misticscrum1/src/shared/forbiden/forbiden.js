cb6503472ccf5a689e4ee60d8b6b01c30a48fa25
Cristina Calle <87840049+callecristina@users.noreply.github.com>
2021-11-11 19:22:08 -0500

    cambios 11/11/2021

--- InterfazUsuario/misticscrum1/src/shared/forbiden/forbiden.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Forbiden() {
-    return (
-        <div>
-            <h1>usuarios sin permiso componente bloqueado</h1>
-        </div>
-    )
-}
