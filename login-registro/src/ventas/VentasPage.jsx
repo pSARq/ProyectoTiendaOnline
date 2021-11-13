222890ab23c8e046a148c3e3f629becf21874214
Anderssohernandez <hernader@gmail.com>
2021-11-13 17:07:32 -0500

    actualizanto

--- login-registro/src/ventas/VentasPage.jsx
+++ login-registro/src/ventas/VentasPage.jsx
@@ -5,74 +5,6 @@ import './registerStyles.css';
 //import apiBseUrl from '../shared/utils/Api';
 
 const VentasPage = () => {
-
-    /*    function producto() {
-            const input1 = document.getElementById("Cantidad").value.toLowerCase();
-            const input2 = document.getElementById("Valor").value.toLowerCase();
-            var valor3 = input1 * input2;
-            document.getElementById("Subtotal").value = valor3;
-        }
-     */
-    /* function genera_tabla() {
-
-        var tblBody = document.querySelector("tbody");
-
-        var hilera = React.createElement("tr");
-
-        var ID = React.createElement("td");
-        var valor1 = document.getElementById("ID").value;
-        //const input2 = document.getElementById('Valor').value.toLowerCase();
-        //var textoCelda = document.createTextNode(valor);
-        ID.appendChild(valor1);
-        hilera.appendChild(ID);
-        tblBody.appendChild(hilera);
-
-        var Descripcion = React.createElement("td");
-        var valor2 = document.getElementById("Descripcion").value;
-        //var textoCelda = document.createTextNode(valor);
-        Descripcion.appendChild(valor2);
-        hilera.appendChild(Descripcion);
-        tblBody.appendChild(hilera);
-
-        var Magnitud = React.createElement("td");
-        var valor3 = document.getElementById("Magnitud").value;
-        //const input1 = document.getElementById('Valor').value.toLowerCase();
-        //var textoCelda = document.createTextNode(valor);
-        Magnitud.appendChild(valor3);
-        hilera.appendChild(Magnitud);
-        tblBody.appendChild(hilera);
-
-        var Cantidad = React.createElement("td");
-        var valor4 = document.getElementById("Cantidad").value;
-        //var textoCelda = document.createTextNode(valor);
-        Cantidad.appendChild(valor4);
-        hilera.appendChild(Cantidad);
-        tblBody.appendChild(hilera);
-
-        var Valor = React.createElement("td");
-        var valor5 = document.getElementById('Valor').value;
-        //const input2 = document.getElementById('Valor').value.toLowerCase();
-        //var textoCelda = document.createTextNode(valor);
-        Valor.appendChild(valor5);
-        hilera.appendChild(Valor);
-        tblBody.appendChild(hilera);
-
-        var Subtotal = React.createElement("td");
-        Subtotal.setAttribute('id', 'totalitem[]');
-        var valor6 = document.getElementById("Subtotal").value;
-        //var textoCelda = document.createTextNode(valor);
-        Subtotal.appendChild(valor6);
-        hilera.appendChild(Subtotal);
-        tblBody.appendChild(hilera);
-
-        var eliminar = React.createElement("td");
-        //eliminar.setAttribute('style', 'text-align: center;');
-        eliminar.innerHTML = '<input type="button" class="btn btn-success"  value="-"/>';
-        hilera.appendChild(eliminar);
-
-    }
- */
-
     return (
         <Fragment >
 
@@ -145,16 +77,7 @@ const VentasPage = () => {
                             <th scope="col" >Subtotal</th>
 
                         </tr>
-                        {/* 
-                        <tr>
-                            <th><input type="text" class="form-control" name="ID" id="ID" /></th>
-                            <th><input class="form-control" type="text" name="Descripcion" id="Descripcion" /></th>
-                            <th><input class="form-control" type="text" name="Magnitud" id="Magnitud" /></th>
-                            <th><input class="form-control" type="number" name="cantidad" id="Cantidad" /></th>
-                            <th><input class="form-control" type="number" name="valor" id="Valor" onKeyUp={producto} /></th>
-                            <th><input class="form-control" disabled="disabled" type="number" name="subtotal" id="Subtotal" /></th>
-                             <th><input type="button" class="btn btn-success" id="adicionar" onclick={genera_tabla} value="+" /></th> 
-                        </tr> */}
+                        
                     </thead>
 
                     <tbody>
