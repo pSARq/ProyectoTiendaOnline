afacbe96ba3c1a326321b6a2ba1b6c8f3f2b6102
Anderssohernandez <hernader@gmail.com>
2021-11-13 16:51:23 -0500

    actualizanto

--- login-registro/src/ventas/VentasPage.jsx
+++ login-registro/src/ventas/VentasPage.jsx
@@ -4,15 +4,15 @@ import './registerStyles.css';
 //import logo from '../assets/img/logo.png';
 //import apiBseUrl from '../shared/utils/Api';
 
- const VentasPage = () => {
+const VentasPage = () => {
 
-/*    function producto() {
-        const input1 = document.getElementById("Cantidad").value.toLowerCase();
-        const input2 = document.getElementById("Valor").value.toLowerCase();
-        var valor3 = input1 * input2;
-        document.getElementById("Subtotal").value = valor3;
-    }
- */
+    /*    function producto() {
+            const input1 = document.getElementById("Cantidad").value.toLowerCase();
+            const input2 = document.getElementById("Valor").value.toLowerCase();
+            var valor3 = input1 * input2;
+            document.getElementById("Subtotal").value = valor3;
+        }
+     */
     /* function genera_tabla() {
 
         var tblBody = document.querySelector("tbody");
@@ -76,30 +76,52 @@ import './registerStyles.css';
     return (
         <Fragment >
 
-            <section class="form-login">
+            <section class="form-venta">
                 {/* <img src="assets/img/logo.png"> */}
                 <h5>Detalle de venta</h5>
+                <div className="datos-venta">
+                    <div className="datos-empresa">
 
-                <div class="form-floating mb-3">
-                    <input type="text" class="form-control" id="floatingDate" placeholder="Date" />
-                    <label for="floatingDate">Fecha</label>
+                        <div class="form-floating mb-3">
+                            <input type="text" class="form-control" id="floatingDate" placeholder="Date" />
+                            <label for="floatingDate">Fecha</label>
+                        </div>
+
+                        <div class="form-floating mb-3">
+                            <input type="text" class="form-control" id="floatingInvoice" placeholder="Invoice" />
+                            <label for="floatingInvoice">Factura</label>
+                        </div>
+
+                        <div class="form-floating mb-3">
+                            <input type="text" class="form-control" id="floatingSeller" placeholder="Seller" />
+                            <label for="floatingSeller">Vendedor</label>
+                        </div>
+
+                    </div>
+
+                    <div className="datos-cliente">
+
+                        <div class="form-floating mb-3">
+                            <input type="text" class="form-control" id="cedula" placeholder="Customer" />
+                            <label for="floatingCustomer">Cedula Cliente</label>
+                        </div>
+
+                        <div class="form-floating mb-3">
+                            <input type="text" class="form-control" id="floatingfirstName" placeholder="First name"
+                            /* onChange={(dataname) => setName(dataname.target.value)} */ />
+                            <label for="floatingFName">First name</label>
+                        </div>
+
+                        <div class="form-floating mb-3">
+                            <input type="text" class="form-control" id="floatinglastName" placeholder="Last name"
+                                /* onChange={(dataLastname) => setLastname(dataLastname.target.value)} */ />
+                            <label for="floatingLName">Last name</label>
+                        </div>
+                    </div>
                 </div>
 
-                <div class="form-floating mb-3">
-                    <input type="text" class="form-control" id="floatingInvoice" placeholder="Invoice" />
-                    <label for="floatingInvoice">Factura</label>
-                </div>
 
-                <div class="form-floating mb-3">
-                    <input type="text" class="form-control" id="floatingSeller" placeholder="Seller" />
-                    <label for="floatingSeller">Vendedor</label>
-                </div>
 
-                <div class="form-floating mb-3">
-                    <input type="text" class="form-control" id="floatingCustomer" placeholder="Customer" />
-                    <label for="floatingCustomer">Cliente</label>
-                </div>
-                
                 <div class="form-floating">
                     <select class="form-select" id="floatingSelect" aria-label="Floating label select example">
                         <option selectd="" value="">Open this menu to select status</option>
@@ -123,7 +145,7 @@ import './registerStyles.css';
                             <th scope="col" >Subtotal</th>
 
                         </tr>
-{/* 
+                        {/* 
                         <tr>
                             <th><input type="text" class="form-control" name="ID" id="ID" /></th>
                             <th><input class="form-control" type="text" name="Descripcion" id="Descripcion" /></th>
@@ -137,14 +159,14 @@ import './registerStyles.css';
 
                     <tbody>
 
-                    <tr>
+                        <tr>
                             <td><input type="text" class="form-control" name="ID" id="ID" /></td>
                             <td><input class="form-control" type="text" name="Descripcion" id="Descripcion" /></td>
                             <td><input class="form-control" type="text" name="Magnitud" id="Magnitud" /></td>
                             <td><input class="form-control" type="number" name="cantidad" id="Cantidad" /></td>
                             <td><input class="form-control" type="number" name="valor" id="Valor" /* onKeyUp={producto} */ /></td>
                             <td><input class="form-control" /* disabled="disabled" */ type="number" name="subtotal" id="Subtotal" /></td>
-                           
+
                         </tr>
                         <tr>
                             <td><input type="text" class="form-control" name="ID" id="ID" /></td>
@@ -153,7 +175,7 @@ import './registerStyles.css';
                             <td><input class="form-control" type="number" name="cantidad" id="Cantidad" /></td>
                             <td><input class="form-control" type="number" name="valor" id="Valor" /* onKeyUp={producto} */ /></td>
                             <td><input class="form-control" /* disabled="disabled" */ type="number" name="subtotal" id="Subtotal" /></td>
-                           
+
                         </tr>
                         <tr>
                             <td><input type="text" class="form-control" name="ID" id="ID" /></td>
@@ -162,7 +184,7 @@ import './registerStyles.css';
                             <td><input class="form-control" type="number" name="cantidad" id="Cantidad" /></td>
                             <td><input class="form-control" type="number" name="valor" id="Valor" /* onKeyUp={producto} */ /></td>
                             <td><input class="form-control" /* disabled="disabled" */ type="number" name="subtotal" id="Subtotal" /></td>
-                           
+
                         </tr>
                         <tr>
                             <td><input type="text" class="form-control" name="ID" id="ID" /></td>
@@ -171,7 +193,7 @@ import './registerStyles.css';
                             <td><input class="form-control" type="number" name="cantidad" id="Cantidad" /></td>
                             <td><input class="form-control" type="number" name="valor" id="Valor" /* onKeyUp={producto} */ /></td>
                             <td><input class="form-control" /* disabled="disabled" */ type="number" name="subtotal" id="Subtotal" /></td>
-                           
+
                         </tr>
                         <tr>
                             <td><input type="text" class="form-control" name="ID" id="ID" /></td>
@@ -179,8 +201,8 @@ import './registerStyles.css';
                             <td><input class="form-control" type="text" name="Magnitud" id="Magnitud" /></td>
                             <td><input class="form-control" type="number" name="cantidad" id="Cantidad" /></td>
                             <td><input class="form-control" type="number" name="valor" id="Valor" /* onKeyUp={producto} */ /></td>
-                            <td><input class="form-control" /* disabled="disabled"  */type="number" name="subtotal" id="Subtotal" /></td>
-                           
+                            <td><input class="form-control" /* disabled="disabled"  */ type="number" name="subtotal" id="Subtotal" /></td>
+
                         </tr>
 
 
@@ -188,10 +210,18 @@ import './registerStyles.css';
 
                 </table>
 
-                <label for="Total">Total: </label>
-                <input class="controls" type="" name="total" value="" />
 
-                <button class="buttons" >Guardar</button>
+
+                <div class="input-group mb-3">
+                    <button class="btn btn-outline-secondary" type="button" id="button-addon1">Total</button>
+                    <input type="text" class="form-control" placeholder="" aria-label="Example text with button addon" aria-describedby="button-addon1" />
+                </div>
+
+
+                <div>
+                    <button type="button" class="btn btn-primary" /* onClick={enviardatos} */ >Guardar Ventas</button>
+                </div>
+
                 {/* <script>
                                                 const boton = document.querySelector
                                                 ('button');
